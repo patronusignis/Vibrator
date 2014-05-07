6391613d1285807f2502bcf702e9b5120082bd80
patronusignis <Fantik97@seznam.cz>
2014-05-07 16:32:20 +0200

    initial commit

---
+++ src/main/java/com/vaclav/kuba/testing/vibrator/app/VibratorActivity.java
@@ -0,0 +1,15 @@
+package com.vaclav.kuba.testing.vibrator.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VibratorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibrator);
+    }
+}
+
+
