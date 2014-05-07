022db730012fcd62ef2d64a8cc40af6ac4a2778e
patronusignis <Fantik97@seznam.cz>
2014-05-07 16:55:13 +0200

    make vibrator running

--- src/main/java/com/vaclav/kuba/testing/vibrator/app/VibratorActivity.java
+++ src/main/java/com/vaclav/kuba/testing/vibrator/app/VibratorActivity.java
@@ -2,6 +2,9 @@ package com.vaclav.kuba.testing.vibrator.app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
 
 public class VibratorActivity extends Activity {
 
@@ -9,6 +12,14 @@ public class VibratorActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vibrator);
+        final Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        Button button = (Button) findViewById(R.id.vibrateButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vibrator.vibrate(500);
+            }
+        });
     }
 }
 
